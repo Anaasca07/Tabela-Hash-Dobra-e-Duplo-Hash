14deae04f2a06accb9edbf5da4c03e8e771125d3
Ana Santana <123104984+Anaasca07@users.noreply.github.com>
2024-04-03 10:19:51 -0300

    Update sistema_de_contatos.c

--- sistema_de_contatos.c
+++ sistema_de_contatos.c
@@ -4,3 +4,11 @@
 
 #define HASH_SIZE 50
 #define MAX_CONTACTS 75
+
+// Estrutura para armazenar um Contact
+typedef struct Contact {
+    char name[50];
+    char phone[20];
+    char email[30];
+    struct Contact *next;
+} Contact;
